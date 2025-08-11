092e13dc74e5f6d86e18862b0b43508cfcd85e1a
Muhammad abrar ahmad <abrarjr66@gmail.com>
2025-08-12 00:00:54 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://abrarjr66:root@cluster0.xl68d.mongodb.net/product-db?retryWrites=true&w=majority&appName=Cluster0')
+  imports: [MongooseModule.forRoot('')
     ,ProductModule], // Other modules will be imported here
   controllers: [AppController], // List of controllers
   providers: [AppService], // Services/providers for DI
